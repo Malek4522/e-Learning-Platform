2be7ef2a090f808e44e736a9f65d088ab706b9ac
Malek4522 <abdelmalek.2004@outlook.com>
2024-12-03 22:38:33 +0100

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,5 +38,5 @@ app.use((err, req, res, next) => {
 // Set port and start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 }); 
